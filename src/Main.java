b21a3808da41861f49d78d37666532b062a5c9fb
Nowadays <herespieceofmine@gmail.com>
2015-10-14 21:06:20 +0200

    Ajout de la classe principal + création de la classe personne

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+/**
+ * Created by Morgan on 14/10/2015.
+ * Project name : GestParapente.
+ */
+public class Main {
+    public static void main(String[] args){
+
+    }
+}
---
+++ src/Person.java
@@ -0,0 +1,11 @@
+/**
+ * Created by Morgan on 14/10/2015.
+ * Project name : GestParapente.
+ */
+public class Person {
+    private String name;
+    private String lastName;
+    public Person(){
+
+    }
+}
